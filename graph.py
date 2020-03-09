7f19f645d87feb7b1b227160ed90ed55f1dc8430
ChuniMuni <tstvitali@gmail.com>
2020-03-09 18:41:54 +0300

    Извлекаем данные, выводим образец формата.

--- graph.py
+++ graph.py
@@ -2,3 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# считаем данные из csv файла
+data = pd.read_csv("covid_19_data.csv")
+
+# определим размер набора данных
+data.shape
+
+# Sample data output
+print(data.head())
